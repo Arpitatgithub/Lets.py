4bf4a809af1bd4e95b11b5f39e14748c349b930b
Arpit Thappa <arpitthaapa8@gmail.com>
2024-12-27 00:04:26 +0530

    Basic operators

---
+++ calculator.py
@@ -0,0 +1,3 @@
+a=10
+b=6
+print("Addition",a,"+", b, "=", a+b,"\n","Subtraction", a,"-",b, "=", a-b, "\n","Multiplication",a,"*",b,"=",a*b, "\n","Division",a ,"/",b,"=",a/b, "\n", "Modulus", a,"%",b,"=",a%b, "\n", "Exponential", a,"**","and",b,"**","=",a**10, b**10, "\n", "Floor Division",a,"//",b, "=",a//b)
