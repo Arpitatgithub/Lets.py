ee4b44c8edc76b8b47a0dea2f8ce678721d49f1d
Arpit Thappa <arpitthaapa8@gmail.com>
2024-12-27 00:43:16 +0530

    functions applied on strings

---
+++ day2.py
@@ -0,0 +1,15 @@
+# x=input("Enter first number")
+# y=input("Enter second number")
+# print(int(x)+ int(y))
+
+# str1="andomando"
+# print(str1[2],"\n")
+# for character in str1:
+#           print(character)
+
+# str1="andomando"
+# print(len(str1), str1[:4])
+
+# str1="andomando"
+# for i in str1:
+#           print(i)
