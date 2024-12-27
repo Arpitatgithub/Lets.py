4a23eed159949fcdf6317f54606bdb45269909b9
Arpit Thappa <arpitthaapa8@gmail.com>
2024-12-27 23:42:40 +0530

    Strings manupilation methods

---
+++ Practice2.py
@@ -0,0 +1,50 @@
+# x=input("Enter first number")
+# y=input("Enter second number")
+# print(int(x)+ int(y))
+
+# str1="andomando"
+# print(str1[2],"\n")
+# for character in str1:
+#           print(character)
+
+# str1="andomando"
+# print(len(str1), str1[:4])
+
+# str1="andomando"
+# for i in str1:
+#           print(i)
+
+# str1="ando"
+# print(str1.upper())
+
+# str1= " andomando "
+# print(str1)
+
+# str1="andomando ;;;; ::::: ][][][][] sidi"
+# print(str1.rstrip("[] sidi"))
+
+# str1="Quick Brown Fox is an independently owned specialty coffee roaster based in New Delhi."
+# print(str1.replace("a","01234"))
+
+# str1="andomando is hii there is a gift "
+# print(str1.split())
+
+# str1="andomando is hii there is a gift "
+# print(str1.capitalize())
+
+# str1="andomando is very good"
+# print(str1.center(200))
+
+# str1="andomando"
+# print(str1.center(20,'*'))
+
+# str1="""Sarah swiftly slices sweet strawberries, sprinkling sugar sensibly. Some soft sunshine streams silently, spreading soothing sensations. Snakes slither silently near streams, searching for snacks. Strong storms suddenly stop, surprising sailors. Stories shared spark smiles, strengthening bonds. Stars shimmer softly, shining secrets. Simple songs soothe souls, spreading serenity swiftly in serene spaces."""
+
+# print(str1.lower().count("s"))
+
+# str1="osdffdffdsdffd"
+# print(str1.endswith("o",0,8))
+
+# str1="andomando is very good"
+# print(str1.find("z"))
+
