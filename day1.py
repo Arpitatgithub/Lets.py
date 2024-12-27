bea0431023f925c83a3a5370fc1c357d3c1a7165
Arpit Thappa <arpitthaapa8@gmail.com>
2024-12-27 23:43:36 +0530

    #

--- day1.py
+++
@@ -1,8 +0,0 @@
-# print("today is 26 december 2026 \nand im start learning python \n" ,end="0001")
-# print("this is my second line")
-# list1= ["andomando", 234, ["hello", 6543]]
-# print(list1)
-# tuple= {(1,2,3,4,),("helo", 6543), ("hii")}
-# print(tuple)
-# dict1= {"name": "Arpit", "age" : 21, "canVote": True}
-# print(dict1)
