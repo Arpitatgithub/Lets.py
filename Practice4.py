4041a1e978ee41949567a3a23f96aef884b56f95
Arpit Thappa <arpitthaapa8@gmail.com>
2024-12-28 00:53:35 +0530

    Loops with If-Else

---
+++ Practice4.py
@@ -0,0 +1,47 @@
+# num=[1,2,3,4,5]
+# for x in num:
+#           print(x)
+
+# for k in range(0,11,2):
+#           print(k) 
+
+# c=int(input("Enter a number"))
+# while (c>0):
+#           print(c)
+#           c=c-1
+
+# numbers=[1,2,3,4,5,6,7,8,9]
+# for i in numbers:
+#           if i%2==0:
+#                   print(f"{i} is even")
+#           else:
+#                   print(f"{i} is odd")
+
+# num=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+# for i in num:
+#           if (i%3 == 0):
+#                   print(f"{i} is a multiplee of 3")
+#           else:
+#                   print(f"{i} is Not a multiple of 3")
+
+# Count down and check if the number is positive
+# num=5
+# while num>-10:
+#           if num>=0:
+#                   print(f"{num} is positive")
+#                   num= num-1
+#           else:
+#                   print(f"{num} is negative") 
+#                   num= num-1    
+
+ # Categorize numbers into ranges
+num=[4,2,7,4,54,-6,-77,0,2,12]
+for i in num:
+          if (i>=0 and i<=5):
+                  print(f"{i} is in between 0 and 5")
+          elif (i>5 and i<=10):
+                  print(f"{i} is in between 6 and 10")
+          elif (i>10):
+                  print(f"{i} is greater then 10")
+          else:
+                  print(f"{i} is a negative number")
