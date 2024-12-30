551fb251a107af4d3e3d196a9f89170335981558
Arpit Thappa <arpitthaapa8@gmail.com>
2024-12-30 23:47:15 +0530

    lambda functions

---
+++ Practice5.py
@@ -0,0 +1,41 @@
+# for r in range(1,10):
+#           if (r==5):
+#                   break
+#           print(r)
+
+# i = 0
+# while True:
+#            i =i+ 1
+#            print(i)
+#            if (i==5):
+#             break
+
+# for i in range(1,10):
+#           if (i==5):
+#                   continue
+#           print(i)
+
+# i=0
+# while i<11:
+#           i=i+1
+          
+#           if (i==6):
+#                   continue
+#           print(i)
+
+# def andomando():
+#           print("you hit andomando")
+# andomando()   
+
+# def sums(a,b):
+#           return a+b
+# a=sums(5,7)
+# print(a)
+
+# def greet(name="User"):
+#           print(f"Hello{name}")
+# greet("Arpit")          
+
+add = lambda a,b:a+b
+print(add(5,6))
+
