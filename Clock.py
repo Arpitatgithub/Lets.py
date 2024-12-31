c4944de4be5ea20df6dcb4fb1f89d2226a9b0a83
Arpit Thappa <arpitthaapa8@gmail.com>
2024-12-31 23:49:38 +0530

    Good morning Arpit

---
+++ Clock.py
@@ -0,0 +1,14 @@
+# import time
+# name=input("Enter your name")
+# def greet():
+#           hour = time.localtime().tm_hour
+
+#           if (5<= hour <12):
+#                   print(f"Good Morning {name}")
+#           elif (12<= hour <17):
+#                   print(f"Good afternoon {name}")  
+#           elif (17<= hour <21):
+#                   print(f"Good evening {name}")
+#           else:
+#                   print(f"Good night {name}") 
+# greet()                                      
