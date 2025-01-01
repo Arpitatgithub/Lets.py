1878002dbbcca71e46e1bd071639b85193bca855
Arpit Thappa <arpitthaapa8@gmail.com>
2025-01-01 23:58:02 +0530

    sets & operations

---
+++ Practice6.py
@@ -0,0 +1,16 @@
+# def factorial(n):
+#           if n == 0:
+#                   return 1
+#           else:
+#                   return n * factorial(n-1)
+# print(factorial(9))      
+ 
+            
+ # Modify recursion limit
+
+num={1,2,3,4}
+# num.add(5)
+
+
+num.update([4,5,6,7])
+print(num)
