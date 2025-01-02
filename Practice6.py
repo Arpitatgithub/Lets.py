abd146361e536858398d4c13aa094e5c95937b36
Arpit Thappa <arpitthaapa8@gmail.com>
2025-01-02 22:10:59 +0530

    Added grades calculator programme

--- Practice6.py
+++ Practice6.py
@@ -34,4 +34,42 @@
 # print(a^b)
 # print(a.symmetric_difference(b)) #symmetric difference of two sets
 
+# math_st={"arpit","aman","ankush","abishek","nishant"}
+# science_st={"arpit","abishek","shreya","nishant"}
+# computer_st={"aman","ankush","shreya","rohit"}
+# print("all students" , math_st|science_st|computer_st )
+# print("students in math and science" , math_st & science_st)
+# print("students in math and computer" , math_st & computer_st)
+# print("students in science and computer" , science_st & computer_st)
+
+# Python Dictionaries
+
+# stu={"name":"Arpit","Age":20}
+# stu["contact"]=9469280356
+# stu["Age"]=21
+# print(stu)
+
+# stu = dict(name="Arpit",age=20, gender="Male", salary=89000)
+# print(stu.pop("age"))
+# del stu["gender"]
+# print(stu)
+
+# for key,value in stu.items():
+#           print(key,":",value)
+
+# grades={}
+# while True:
+#           actions=input("'add','view ','quit'")
+#           if actions=='add':
+#                   name= input("Enter student name :")
+#                   grade=input("Enter grade :")
+#                   grades[name]=grade
+#           elif actions=='view':
+#                   for key,value in grades.items():
+#                           print(key,":",value)
+#           elif actions=='quit' :
+#                   break
+#           else:
+#                   print("invalid input")
+# print(grades)                                         
 
