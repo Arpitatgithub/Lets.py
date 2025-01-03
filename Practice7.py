ce48c1a591c3566e6733f67b7705654c4929593a
Arpit Thappa <arpitthaapa8@gmail.com>
2025-01-03 19:40:18 +0530

    added import

--- Practice7.py
+++ Practice7.py
@@ -1,4 +1,4 @@
-fruits=["apple","banana","cherry","mango"]
+# fruits=("apple","banana","cherry","mango")
 # index=0
 # for i in fruits:
 #           print(index,i)
@@ -10,8 +10,15 @@ fruits=["apple","banana","cherry","mango"]
 # for index,i in enumerate(fruits,start=2):
 #           print(index,i)
 
+# Creating a dictionary from a list
+ 
+# fruit_list=(i for i in enumerate(fruits))
+# fruit_list=[index for index in enumerate(fruits)]
+# fruit_dict={index:i for index,i in enumerate(fruits) }
+# print(fruit_dict,"\n",fruit_list,"\n",fruits)
 
+import math
 
-
+# print(dir(math))
 
 
