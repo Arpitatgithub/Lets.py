5176b2b42362d838bc13038d186cd2a009cc63a4
Arpit Thappa <arpitthaapa8@gmail.com>
2025-01-03 19:15:11 +0530

    Enumerate in python

---
+++ Practice7.py
@@ -0,0 +1,17 @@
+fruits=["apple","banana","cherry","mango"]
+# index=0
+# for i in fruits:
+#           print(index,i)
+#           index+=1
+
+# for index,a in enumerate(fruits):
+#           print(index,a)
+
+# for index,i in enumerate(fruits,start=2):
+#           print(index,i)
+
+
+
+
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,2 @@
-# Example Usage
-# if __name__ == "__main__":
-#     original_message = "hello world hi python"
-#     print("Original Message:", original_message)
-
-#     encoded_message = encode_message(original_message)
-#     print("Encoded Message:", encoded_message)
-
-#     decoded_message = decode_message(encoded_message)
-#     print("Decoded Message:", decoded_message)
+for index in fruits:
+          print(index)
