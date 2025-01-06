658d310a750a4852b7faf72aac302d54f74ee82a
Arpit Thappa <arpitthaapa8@gmail.com>
2025-01-06 17:44:41 +0530

    is and == operators

---
+++ Practice8.py
@@ -0,0 +1,15 @@
+# Difference between is and == in Python
+
+
+# a=[1,2,3]
+# a=[4,5,6]
+# # print(a == b)
+# print(a is a)
+
+# a=[1,2]
+# b=a
+# print(a is b)
+
+a=5.387654
+b=5.387654
+print(a is b)
