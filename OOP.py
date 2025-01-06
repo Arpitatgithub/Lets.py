6671333fd648aae0baaaa2ecab722fdf9742ccd7
Arpit Thappa <arpitthaapa8@gmail.com>
2025-01-06 21:13:56 +0530

    Introduction to OOP

---
+++ OOP.py
@@ -0,0 +1,243 @@
+# class data:
+#           name="Arpit"
+#           age=20
+# data()          
+
+# class Person:
+#     # Constructor method (initializer)
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     # Method to display information
+#     def display(self):
+#         print(f"Name: {self.name}, Age: {self.age}")
+
+
+# # Creating an object
+# person1 = Person("Alice", 25)
+# person2 = Person("Bob", 30)
+
+# # Accessing methods
+# person1.display()
+# person2.display()
+
+# class Dog:
+#     def __init__(self, name, breed):
+#         self.name = name
+#         self.breed = breed
+
+# dog1 = Dog("Buddy,", "Golden Retriever")
+# print(dog1.name, dog1.breed)
+
+
+
+# class animal:
+#     def __init__ (self,name,breed):
+#         self.name = name
+#         self.breed = breed
+
+# dog1= animal("Simba","Indian")
+# print(dog1.name,dog1.breed)
+
+
+# class report:
+#           def __init__(self,marks,grades):
+#                   self.marks=marks
+#                   self.grades=grades
+
+# Final_grades=report(72,"B+")
+# print(Final_grades.marks,Final_grades.grades)
+
+class index:
+          def __init__(self,Sno,Content):
+                  self.sno=Sno
+                  self.content=Content
+
+Index_1=index(92,"Daffodills")
+print(Index_1.sno,Index_1.content)
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
