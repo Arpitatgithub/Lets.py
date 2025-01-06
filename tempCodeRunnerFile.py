bd0de01f90444a9502e128430edc9248e75bd6f4
Arpit Thappa <arpitthaapa8@gmail.com>
2025-01-06 17:45:11 +0530

    #

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-num=[1,2,3,4,5,6]
-filtered=list(filter(lambda x:x%2==0,num))
-square=list(map(lambda x: x**2,filtered))
-sum=(reduce(lambda x,y:x+y,square))
-print(filtered,square,sum)
+print(a == b)
