8afb5c3167db4ef1d2759e34deef7f19dc4b1904
Arpit Thappa <arpitthaapa8@gmail.com>
2025-03-01 02:43:11 +0530

    #

---
+++ classes.py
@@ -0,0 +1,35 @@
+# class Student:
+#           name="Ayush"
+#           age=19
+#           classs=11
+
+# a= Student()
+# a.name="Arpit"
+# print(a.name)
+
+
+
+class Student:
+          name="Ayush"
+          age=19
+          classs=11
+          def info(self):
+                  print(f"{self.name} is {self.age} years old")
+
+a= Student()
+b= Student()
+c= Student()
+d= Student()
+
+a.name="Arpit"
+a.age=12
+
+b.name="Simran"
+b.age=20
+
+c.name="Muskan"
+c.age=18
+
+a.info()
+b.info()
+c.info()
